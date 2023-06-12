879f435478499201b8217766abcab3d660f7e888
soywiz <soywiz@gmail.com>
2023-06-12 16:20:46 +0200

    KorGE 4.0.5a

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import korlibs.korge.gradle.*
 
 plugins {
     //id("com.soywiz.korge") version "999.0.0.999"
-    id("com.soywiz.korge") version "4.0.0-rc4"
+    id("com.soywiz.korge") version "4.0.5a"
     id("org.jetbrains.compose") version "1.4.0"
 }
 
